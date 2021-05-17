7510a96bf13ae1ae62aab6711bc791a0d75eeee3
Matthew Cook <matthewcook13987@gmail.com>
2021-05-17 15:42:31 -0500

    elsif

---
+++ elseif.py
@@ -0,0 +1,5 @@
+password = 'fish'
+if password == 'fish':
+  print('yes')
+else:
+  print('no')
---
+++ name.py
@@ -0,0 +1,6 @@
+print('Enter a name.')
+name = input()
+if name != '':
+  print('Thanks for entering your name, ' + name)
+else:
+  print('You did not enter a name')
