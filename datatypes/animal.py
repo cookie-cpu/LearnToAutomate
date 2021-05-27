66fac6dc4d2ab5ac00f7c3008a2fe8a5d9a120c0
Matthew Cook <matthewcook13987@gmail.com>
2021-05-27 15:05:56 -0500

    adds animal list practice

---
+++ datatypes/animal.py
@@ -0,0 +1,18 @@
+animals = ['cat','dog','bird','rat','mouse']
+my_list = [['cat','dog','bird'], [1,2,3,4,5,'last item']]
+print(animals[2])
+print(my_list[0])
+print(my_list[0][1])
+print(my_list[1][1])
+print(my_list[1])
+print(my_list[1][-1])
+print(animals[1:4])
+print('The ' + animals[-1] + ' is afraid of the ' + animals[0])
+print(animals[:2])
+print(animals[2:])
+print(animals)
+del animals[2]
+print(animals)
+print(list('hello'))
+print(3 in [0,5,10,])
+print(1 in [1,2,3])
