6ab1dbd12d5b06f09b3104353594b3b58c0e076f
Matthew Cook <matthewcook13987@gmail.com>
2021-05-27 15:23:58 -0500

    list methods

--- lists/methods.py
+++ lists/methods.py
@@ -1,2 +1,17 @@
-spam = [1,2,3,4,5,6,7,8,9,10]
-print(spam.index(5))
+numbers = [1,2,3,4,5,6,7,8,9,10]
+print(numbers.index(5))
+numbers.append('definitely a number not a string')
+print(numbers)
+numbers.insert(5, 'spaghetti')
+print(numbers)
+numbers.remove(2)
+print(numbers)
+
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f']
+letters.sort(reverse=True)
+print(letters)
+
+spam = ['A','Z','a','z']
+spam.sort(key=str.lower)
+print(spam)
